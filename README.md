fcd8ff325d92f387b6d1236ed5bf346564c8bc93
Chris Porter <chris.porter@uplight.com>
2022-03-01 22:44:13 -0800

    Copying latest Spellie into wordlekids.com

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-# wordle-kids
+# Wordle Kids
 
-Simple redirect to spelliegame.com
+A copy of [Spellie](https://spelliegame.com) but at a new domain that hopefully won't run into aggressive DNS blocking at schools.
+
+[Try it out](https://wordlekids.com)
+
---
+++ types.d.ts
@@ -0,0 +1,59 @@
+export type GuessState = "pending" | "match" | "present" | "miss";
+
+export type LetterGuess = {
+  letter: string;
+  state: GuessState;
+};
+
+export type Guess = LetterGuess[];
+
+export type Hint = {
+  /** to display to user */
+  message: string;
+  /** to highlight on the keyboard */
+  letter?: string;
+  /** letters that are definitely not in the target */
+  misses?: string[];
+};
+
+export type KeyState = {
+  /** most keys are letters but one is backspace, one is to submit a guess */
+  type: "letter" | "back" | "enter";
+  /** shown on the keyboard */
+  label: string;
+  /** keys start as "available" and update as user guesses (based on presence in the target word) */
+  state: "available" | "match" | "present" | "miss";
+};
+
+export type Knowledge = {
+  matches?: string[];
+  presents?: string[];
+  misses?: string[];
+  availables?: string[];
+};
+
+export type Settings = {
+  difficulty: "easy" | "medium" | "hard";
+  keyboardLayout: "atoz" | "qwerty";
+  theme: "blue" | "high";
+  case: "lowercase" | "uppercase";
+  sharingEmojis: EmojiMatchThemeKey;
+  spellChecker: "off" | "on";
+};
+
+export type EmojiMatchThemeKey =
+  | "nature"
+  | "fruit"
+  | "vegetable"
+  | "tree"
+  | "circle";
+
+export type EmojiMatchThemeValue = {
+  match: string;
+  present: string;
+  miss: string;
+};
+
+export type EmojiMatchThemes = {
+  [key in EmojiMatchThemeKey]: EmojiMatchThemeValue;
+};
